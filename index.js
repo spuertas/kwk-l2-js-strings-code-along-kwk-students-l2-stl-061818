b0fa38e13a42ca6f8cef4a6355abb159c9f55c12
Sophia <spuertas@micds.org>
2018-06-20 20:16:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 let currentUser = "me";
 let welcomeMessage = "Welcome to Klossybook, !";
 let excitedWelcomeMessage = "WELCOME TO KLOSSYBOOK, !"
-let shortGreeting = "Welcome, ${currentUser(0)} !"
+let shortGreeting = "Welcome, " + currentUser(0) + "!";
