564c30297ea05e5ea01d552e329bc7ff4c77a5a5
Sophia <spuertas@micds.org>
2018-06-20 20:25:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 // Write your code in this file!
-
+let currentUser = "meee";
+let welcomeMessage = `Welcome to Klossybook, ${currentUser} !`;
+let excitedWelcomeMessage = `WELCOME TO KLOSSYBOOK, ${currentUser.toUpperCase()}!`
+let shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
